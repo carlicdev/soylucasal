344e6e3d277dbd558c259e4361d31d6ea487b9f5
carlos camacho <carlic.dev@gmail.com>
2024-05-07 11:41:58 -0600

    changed author card

--- app/components/AuthorSideCard.js
+++ app/components/AuthorSideCard.js
@@ -5,7 +5,7 @@ const AuthorSideCard = () => {
       <div className='card p-3 rounded-lg flex flex-col justify-center z-10'>
           <Image src={'/man.png'} width={100} height={100} className="mx-auto"/>
           <p className="text-2xl font-black my-2">Quién es Luca?</p>
-          <p className='mb-4 font-light'>Soy escritor, programador y emprendedor. <span className="font-bold">Comprometido con ayudarte a construir tu <span className="text-yellow-500">vida ideal</span>.</span></p>
+          <p className='mb-4 font-light'>Soy escritor, programador y emprendedor. También soy un monstruo en el poker.</p>
       </div>
     )
   }
