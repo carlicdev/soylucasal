3d5e1695de3f3c91d4d2ddcd0341a62c9df7f4b5
carlos camacho <carlic.dev@gmail.com>
2024-05-14 13:10:03 -0600

    change confirmation

--- app/confirmation/page.js
+++ app/confirmation/page.js
@@ -6,7 +6,7 @@ const ConfirmationPage = () => {
       <main className='h-screen pt-[110px]'>
           <div className='max-w-5xl mx-auto card p-5 flex flex-col gap-4'>
               <p className='text-5xl font-black'>Quiero agradecerte por la confianza.</p>
-              <p className='text-5xl font-black'>Estás en el <span className="text-yellow-500">camino correcto</span> para acceder al <span className="text-yellow-500">1%</span>.</p>
+              <p className='text-5xl font-black'>Bienvenido a  <span className="text-yellow-500">La Manada.</span></p>
               <p className='text-2xl'>En tu bandeja de entrada encontrarás un correo de bienvenida, así como una descripción general de lo que puedes esperar de este Newsletter.</p>
               <p className='text-2xl mt-5'>¿Quieres leer ediciones previas de mi Newsletter? Puedes leer todos los árticulos anteriores en mi Blog.</p>
               <div className='flex'>
